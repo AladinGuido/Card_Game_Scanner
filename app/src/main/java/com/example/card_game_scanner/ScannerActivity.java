91f7b3171d41704fc06fe19fa01987ba5c2520d9
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-17 01:41:30 +0800

    Added Camera functionality

--- app/src/main/java/com/example/card_game_scanner/ScannerActivity.java
+++ app/src/main/java/com/example/card_game_scanner/ScannerActivity.java
@@ -4,11 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ScannerActivity extends AppCompatActivity {
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ScannerActivity extends CameraActivity {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                return inputFrame.rgba();
+            }
+        });
+
+        if(OpenCVLoader.initDebug())
+        {
+            cameraBridgeViewBase.enableView();
+        }
+
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraBridgeViewBase.enableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraBridgeViewBase.disableView();
     }
 }
