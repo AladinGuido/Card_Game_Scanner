0f77bdf5626cece97dbf8448508938a5886938d4
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-16 20:37:04 +0800

    added function to button on main activity and started the scanner activity

--- app/src/main/java/com/example/card_game_scanner/MainActivity.java
+++ app/src/main/java/com/example/card_game_scanner/MainActivity.java
@@ -2,19 +2,39 @@ package com.example.card_game_scanner;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Set views
+    Button buttonStartScanning;
+
+    //Set possible activities
+    Intent scannerActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = findViewById(R.id.button_ScanButton);
+        //Get views
+        buttonStartScanning = findViewById(R.id.button_ScanButton);
 
+        //Get possible activities
+        scannerActivity = new Intent(this, ScannerActivity.class);
+
+
+        buttonStartScanning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(scannerActivity);
+            }
+        });
     }
 }
