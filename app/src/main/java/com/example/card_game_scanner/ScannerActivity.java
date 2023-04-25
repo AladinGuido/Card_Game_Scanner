bc56bff95a49368d8c5cb45a50a0ce78c2157386
GuidoPerren <andringuido.perren@my.jcu.edu.au>
2023-04-25 16:41:28 +0800

    created function to create an image were the borders are outlined for further usage.

--- app/src/main/java/com/example/card_game_scanner/ScannerActivity.java
+++ app/src/main/java/com/example/card_game_scanner/ScannerActivity.java
@@ -2,12 +2,19 @@ package com.example.card_game_scanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 
 import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,6 +27,7 @@ public class ScannerActivity extends CameraActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
 
         cameraBridgeViewBase = findViewById(R.id.cameraView);
 
@@ -36,7 +44,26 @@ public class ScannerActivity extends CameraActivity {
 
             @Override
             public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-                return inputFrame.rgba();
+                Mat imageOutput = inputFrame.rgba();
+                Imgproc.cvtColor(imageOutput, imageOutput, Imgproc.COLOR_BGR2GRAY);
+
+                Mat blurredImage = new Mat();
+                Size kernelSize = new Size(15, 15);
+                Imgproc.GaussianBlur(imageOutput, blurredImage, kernelSize, 0);
+
+                int img_w = imageOutput.cols(); // Get image width
+                int img_h = imageOutput.rows(); // Get image height
+                int bkg_level = (int)imageOutput.get(img_h / 100, img_w / 2)[0]; // Get pixel value at 1% of image height and center of image width
+                int BKG_THRESH = 50; // Define threshold value
+                int thresh_level = bkg_level + BKG_THRESH; // Calculate threshold level
+
+
+                Mat thresh = new Mat();
+                Core.inRange(blurredImage, new Scalar(thresh_level), new Scalar(255), thresh);
+
+
+
+                return getImageThreshold(inputFrame);
             }
         });
 
@@ -68,5 +95,28 @@ public class ScannerActivity extends CameraActivity {
     protected void onDestroy() {
         super.onDestroy();
         cameraBridgeViewBase.disableView();
+    }
+
+    Mat getImageThreshold(CameraBridgeViewBase.CvCameraViewFrame frame)
+    {
+        int BACKGROUND_THRESHOLD = 50;
+
+
+        Mat grayImage = new Mat();
+        Imgproc.cvtColor(frame.rgba(), grayImage, Imgproc.COLOR_BGR2GRAY);
+
+        Mat blurredImage = new Mat();
+        Size kernelSize = new Size(15, 15);
+        Imgproc.GaussianBlur(grayImage, blurredImage, kernelSize, 0);
+
+        int imageWidth = grayImage.cols();
+        int imageHeight = grayImage.rows();
+        int backgroundLevel = (int) grayImage.get(imageHeight / 100, imageWidth / 2)[0];
+        int thresholdValue = backgroundLevel + BACKGROUND_THRESHOLD;
+
+        Mat thresholdedImage = new Mat();
+        Imgproc.threshold(blurredImage, thresholdedImage, thresholdValue, 255, Imgproc.THRESH_BINARY);
+
+        return thresholdedImage;
     }
 }
